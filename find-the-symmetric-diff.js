88f4a9a26723c7945895f8e923e0ad133ab90e3e
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-09-14 22:22:56 +0900

    Used reduce

--- find-the-symmetric-diff.js
+++ find-the-symmetric-diff.js
@@ -1,8 +1,5 @@
 function sym(...args) {
-  const noDoubles = args.map((item) => {
-    return [...new Set(item)];
-  });
-  function getDiffNums(a, b) {
+  function getDiffNums([...a], [...b]) {
     let result = [];
     a.forEach((item) => {
       const indexOf = b.indexOf(item);
@@ -14,12 +11,9 @@ function sym(...args) {
     });
     return [...result, ...b];
   }
-  let result = [];
-  const [firstArg] = noDoubles.splice(0, 1);
-  result = firstArg;
-  for (let index = 0; index < noDoubles.length; index++) {
-    result = [...getDiffNums(result, noDoubles[index])];
-  }
+  const result = args
+    .map((arg) => [...new Set(arg)])
+    .reduce(getDiffNums(previousValue, currentValue), []);
   return result;
 }
 
