028a2de7c2e1a751669a665f2e91633db65916fc
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-09-22 21:14:43 +0900

    Removed map

--- find-the-symmetric-diff.js
+++ find-the-symmetric-diff.js
@@ -1,20 +1,16 @@
 function sym(...args) {
-  function getDiffNums([...a], [...b]) {
-    let result = [];
-    a.forEach((item) => {
-      const indexOf = b.indexOf(item);
-      if (indexOf > -1) {
-        b.splice(indexOf, 1);
-        return;
+  const result = args.reduce((currentVal, previousVal) => {
+    const noDuplicates = [...new Set(previousVal)]
+    noDuplicates.forEach(val => {
+      if (currentVal.has(val)) {
+        currentVal.delete(val)
+        return
       }
-      result.push(item);
-    });
-    return [...result, ...b];
-  }
-  const result = args
-    .map((arg) => [...new Set(arg)])
-    .reduce(getDiffNums(previousValue, currentValue), []);
-  return result;
+      currentVal.add(val)
+    })
+    return currentVal
+  }, new Set());
+  return [...result]
 }
 
 console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]));
